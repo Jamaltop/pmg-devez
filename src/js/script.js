a2bdc9c5f6ce40a4068f55afb1fd7582645936bb
Camal <camalkos91@gmail.com>
2025-11-05 21:37:26 +0400

    закончил все что можно было кроме абаут ас

--- src/js/script.js
+++ src/js/script.js
@@ -11,11 +11,12 @@ const arrow2 = document.querySelector('.header__menu-arrow-2');
 const dropdownList1 = document.querySelector('.header__menu-dropdown-list1');
 const dropdownList2 = document.querySelector('.header__menu-dropdown-list2');
 
-dropdown1.addEventListener('click', () => {
-    arrow1.classList.toggle('rotate-180');
-    dropdownList1.classList.toggle('active');
-    dropdownList1.style.animation = 'dropdown-animation-open 0.3s ease';
-});
+
+arrow1.addEventListener('click' ,() => {
+  arrow1.classList.toggle('rotate-180');
+  dropdownList1.classList.toggle('active');
+  dropdownList1.style.animation = 'dropdown-animation-open 0.3s ease';
+})
 dropdown2.addEventListener('click', () => {
     arrow2.classList.toggle('rotate-180');
     dropdownList2.classList.toggle('active');
